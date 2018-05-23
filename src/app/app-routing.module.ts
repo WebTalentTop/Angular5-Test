f0e8d7a5762a783c8d74fb6caa079de9b8bf4be2
Rene <toptal.alltime@gmail.com>
2018-05-23 07:26:42 -0400

    move 2 Pages to Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FirstPageComponent } from './first-page/first-page.component';
-import { SecondPageComponent } from './second-page/second-page.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import { SecondPageComponent } from './modules/second-page/second-page.component';
 
 const routes: Routes = [
-  { path: 'first-page', component: FirstPageComponent},
-  { path: 'second-page', component: SecondPageComponent},
+  { path: 'home', component: HomePageComponent},
+  { path: 'second', component: SecondPageComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { SidenavComponent } from './core/side-nav/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { FirstPageComponent } from './first-page/first-page.component';
-import { SecondPageComponent } from './second-page/second-page.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import { SecondPageComponent } from './modules/second-page/second-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
-    FirstPageComponent,
+    HomePageComponent,
     SecondPageComponent,
   ],
   imports: [

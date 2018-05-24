1bf4214b758841d332acd0776a7c02a7665dfbb3
Rene <toptal.alltime@gmail.com>
2018-05-23 22:39:44 -0400

    add a Dialog button to Sidebar

--- src/app/core/side-nav/side-nav.component.ts
+++ src/app/core/side-nav/side-nav.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { ToolbarComponent } from '../tool-bar/tool-bar.component';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ToolbarDialog } from '../../ui/toolbar-dialog/toolbar-dialog.component';
 @Component({
   selector: 'side-nav',
   templateUrl: './side-nav.component.html',
@@ -10,5 +11,15 @@ import { Observable } from 'rxjs';
 })
 export class SidenavComponent {
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, public dialog: MatDialog) {}
+  openDialog() {
+    const dialogRef = this.dialog.open(ToolbarDialog, {
+      width: '250px',
+      height: '150px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog closed`);
+    });
+  }
 }
